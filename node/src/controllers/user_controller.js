212aa32503d30ae153d9d2ab8e492c5009fbbec9
AMIR BHATTARAI <amirgulmeli123@gmail.com>
2026-01-21 13:03:06 +0545

    CRUD

--- node/src/controllers/user_controller.js
+++ node/src/controllers/user_controller.js
@@ -1,11 +1,17 @@
-export const getUsers = (req, res) => {
+import User from "../models/user.model.js"
+export const getUsers = async(req, res , next) => {
+  try{
+  const users = await User.find()
   res.status(200).json({
     message: "User fetched",
-    data: [],
+    data: users,
     success: true,
   });
-};
 
+} catch(error){
+  next(error)
+}
+};
 export const create = (req, res) => {
   console.log(req.body);
   res.status(201).json({
@@ -27,16 +33,43 @@ export const update = (req, res) => {
   });
 };
 
-export const getByid = (req, res) => {
+export const getByid = async(req, res , next) => {
+  try{
   const id = req.params.id;
-
+  // const email = req.params.id;
+  const user = await User.findById(id)
+  // other method
+  // const user = await User.findone({email:email});
+  if(!user){
+    next({
+      status :404,
+      message : "User not Found"
+    })
+  }
   res.status(200).json({
     message: `get user by id ${id}`,
+    data : user
   });
+}catch (error){
+  next(error);
+}
 };
 
-export const remove = (req, res) => {
+export const remove = async(req, res , next) => {
+  try{
+  const id = req.params.id;
+  const user = await User.findOne({_id : id});
+  if(!user){
+    next({
+      message : "User not found!",
+      status:400
+    })
+    return
+  }
   res.status(200).json({
     message: `id: ${req.params.id} deleted`,
   });
+}catch(error){
+  next(error)
+}
 };
